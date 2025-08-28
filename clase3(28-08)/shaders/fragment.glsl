07a262d62cdbeda7b6d4a5d2e16339934e150bbf
delfisalinasmich <delfina.bircleai@gmail.com>
2025-08-28 17:26:01 -0300

    Add fragment shader implementation to handle color output with alpha transparency

---
+++ clase3(28-08)/shaders/fragment.glsl
@@ -0,0 +1,11 @@
+#version 330
+
+// recibimos lo que viene del vertex
+in vec3 v_color;
+
+// salida del fragment a renderizar
+out vec4 f_color; // (r, g, b, alpha) los colores
+
+void main(){
+    f_color = vecr(v_color, 1.0); //agrego una coordenada mas para el alpha, porque el vertice no tiene trasnparencia
+}
