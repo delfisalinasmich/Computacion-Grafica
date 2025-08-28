6469ee21cd214d9ad07ac0de2a055182204aab4a
delfisalinasmich <delfina.bircleai@gmail.com>
2025-08-28 16:53:12 -0300

    Refactor window initialization and context creation in tp3.py

--- clase3(28-08)/tp3.py
+++ clase3(28-08)/tp3.py
@@ -1,3 +1,12 @@
 import pyglet # For windowing and user input
 import moderngl # For OpenGL context and rendering
 import numpy as np # For numerical operations
+
+class Window (pyglet.window.Window): #ventana
+    def __init__(self):
+        super().__init__(640, 480, "TP3 - Salinas") #ancho por alto y nombre de ventana
+        # crear mi canvas ctx = contexto
+        self.ctx = moderngl.create_context() #contexto de OpenGL / canvas new canvas de cpu
+
+Window() # crear una instancia de la clase window
+pyglet.app.run() #loop de eventos
