cbf80af7a9ae7409ae1495d7a03b5dc3458cbd02
delfisalinasmich <delfina.bircleai@gmail.com>
2025-09-25 10:05:55 -0300

    Refactor Cube class: reorder imports and enhance get_model_matrix method for improved transformation handling

--- TricherriSantiago_SalinasDelfina/src/cube.py
+++ TricherriSantiago_SalinasDelfina/src/cube.py
@@ -1,6 +1,6 @@
-from hit import HitBox, HitBoxOBB
-import numpy as np
 import glm
+import numpy as np
+from hit import HitBoxOBB
 
 class Cube:
     def __init__(self, position=(0,0,0), rotation=(0,0,0), scale=(1,1,1), name="cube"):
@@ -10,7 +10,7 @@ class Cube:
         self.scale = glm.vec3(*scale)
         self.__colision = HitBoxOBB(get_model_matrix=lambda: self.get_model_matrix())
 
-        # Agrega los vértices y los índices de un cubo
+        # Vértices y colores del cubo
         self.vertices = np.array([
             # x, y, z,   r, g, b
             -1, -1, -1,  1, 0, 0,
@@ -31,3 +31,15 @@ class Cube:
             3, 2, 6, 6, 7, 3,  # cara superior
             0, 1, 5, 5, 4, 0,  # cara inferior
         ], dtype='i4')
+
+    def check_hit(self, origin, direction):
+        return self.__colision.check_hit(origin, direction)
+
+    def get_model_matrix(self):
+        model = glm.mat4(1)
+        model = glm.translate(model, self.position)
+        model = glm.rotate(model, glm.radians(self.rotation.x), glm.vec3(1, 0, 0))
+        model = glm.rotate(model, glm.radians(self.rotation.y), glm.vec3(0, 1, 0))
+        model = glm.rotate(model, glm.radians(self.rotation.z), glm.vec3(0, 0, 1))
+        model = glm.scale(model, self.scale)
+        return model
