33fdbcdaf484bdcf6eb7b8f3108170825439f498
delfisalinasmich <delfina.bircleai@gmail.com>
2025-09-25 10:04:23 -0300

    Refactor Cube class to include vertex and index definitions for rendering; update main.py to dynamically set shader paths.

--- TricherriSantiago_SalinasDelfina/src/cube.py
+++ TricherriSantiago_SalinasDelfina/src/cube.py
@@ -10,14 +10,24 @@ class Cube:
         self.scale = glm.vec3(*scale)
         self.__colision = HitBoxOBB(get_model_matrix=lambda: self.get_model_matrix())
 
-    def check_hit(self, origin, direction):
-        return self.__colision.check_hit(origin, direction)
+        # Agrega los vértices y los índices de un cubo
+        self.vertices = np.array([
+            # x, y, z,   r, g, b
+            -1, -1, -1,  1, 0, 0,
+             1, -1, -1,  0, 1, 0,
+             1,  1, -1,  0, 0, 1,
+            -1,  1, -1,  1, 1, 0,
+            -1, -1,  1,  1, 0, 1,
+             1, -1,  1,  0, 1, 1,
+             1,  1,  1,  1, 1, 1,
+            -1,  1,  1,  0, 0, 0,
+        ], dtype='f4')
 
-    def get_model_matrix(self):
-        model = glm.mat4(1)
-        model = glm.translate(model, self.position)
-        model = glm.rotate(model, glm.radians(self.rotation.x), glm.vec3(1, 0, 0))
-        model = glm.rotate(model, glm.radians(self.rotation.y), glm.vec3(0, 1, 0))
-        model = glm.rotate(model, glm.radians(self.rotation.z), glm.vec3(0, 0, 1))
-        model = glm.scale(model, self.scale)
-        return model
+        self.indices = np.array([
+            0, 1, 2, 2, 3, 0,  # cara trasera
+            4, 5, 6, 6, 7, 4,  # cara delantera
+            0, 4, 7, 7, 3, 0,  # cara izquierda
+            1, 5, 6, 6, 2, 1,  # cara derecha
+            3, 2, 6, 6, 7, 3,  # cara superior
+            0, 1, 5, 5, 4, 0,  # cara inferior
+        ], dtype='i4')
--- TricherriSantiago_SalinasDelfina/src/main.py
+++ TricherriSantiago_SalinasDelfina/src/main.py
@@ -4,9 +4,17 @@ from cube import Cube
 from camera import Camera
 from scene import Scene
 
+import os
+
 # --- Loop principal ---
 window = Window(800, 600, "Basic Graphic Engine")
-shader_program = ShaderProgram(window.ctx, 'shaders/basic.vert', 'shaders/basic.frag')
+
+# Obtener ruta absoluta a los shaders
+shader_dir = os.path.join(os.path.dirname(__file__), '..', 'shaders')
+vertex_shader_path = os.path.join(shader_dir, 'basic.vert')
+fragment_shader_path = os.path.join(shader_dir, 'basic.frag')
+
+shader_program = ShaderProgram(window.ctx, vertex_shader_path, fragment_shader_path)
 cube1 = Cube((-2, 0, 0), (0, 45, 0), (1, 1, 1), name="Cube1")
 cube2 = Cube((2, 0, 0), (0, 45, 0), (1, 0.5, 1), name="Cube2")
 camera = Camera((0, 0, 6), (0, 0, 0), (0, 1, 0), 45, window.width / window.height, 0.1, 100.0)
